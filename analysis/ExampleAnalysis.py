3d0cc153db876057fb432368034b6f62f4709f23
Karol Krizka <kkrizka@gmail.com>
2024-07-16 13:35:12 +0100

    Add support for TH2.

--- analysis/ExampleAnalysis.py
+++ analysis/ExampleAnalysis.py
@@ -8,9 +8,13 @@ class ExampleHistograms(Analysis.Histograms):
 
         self.hist_dimuonPt=ROOT.TH1F("dimuon_pt", f'{self.title};di-muon p_{{T}} [GeV];a.u.', 50, 0, 5000)
 
+        self.hist_muon1Ptvsmuon0Pt=ROOT.TH2F("muon1vsmuon0_pt", f'{self.title};leading muon p_{{T}} [GeV];subleading muon p_{{T}} [GeV];', 50, 0, 5000, 50, 0, 5000)
+
     def fill_histograms(self, sample):
         self.hist_muon0Pt.Fill(sample.Muon[0].PT)
         self.hist_muon1Pt.Fill(sample.Muon[1].PT)
+
+        self.hist_muon1Ptvsmuon0Pt.Fill(sample.Muon[0].PT,sample.Muon[1].PT)
 
         # Add muons using pT
         dimuon=sample.Muon[0].P4()+sample.Muon[1].P4()
